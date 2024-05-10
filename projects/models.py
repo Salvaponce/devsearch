59521d7a13da266d5c6e89650eb8dfd8ce5e9251
Salvador Ponce <salvaponce92@gmail.com>
2024-05-10 19:19:25 +0200

    first commit

---
+++ projects/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+import uuid
+
+# Create your models here. Data base
+
+class Project(models.Model): #We inherance from models. Meaning we create a officiallly a django model
+    #owner =
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) #null is for the db. blanck is for django.
+    #featured_image =
+    demo_link = models.CharField(max_length=1200, null=True, blank=True)
+    source_link = models.CharField(max_length=1200, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True) #Is a string because the model Tag is below this model
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+
+    VOTE_TYPE = (
+        ('up', 'up'),
+        ('down', 'down')
+    )
+
+    #owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.project.title + " || " + self.value
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
---
+++ projects/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
+
+# Create your views here. Logic of our app
+#Can respond with an html or json or whatever you want
+
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/projects.html', {"projects": projects})
+
+def project(request, pk):
+    projectObj = Project.objects.get(id = pk)
+    tags = projectObj.tags.all()
+    reviews = projectObj.review_set.all() #Go to this project and give all the children, give the entire set
+    context = {"project": projectObj, "tags": tags, "reviews": reviews}
+    return render(request, 'projects/single-project.html', context)
+
+def createProject(request):
+    if request.method == 'POST':  # Check if it's a POST request
+        form = ProjectForm(request.POST)  # Pass POST data to the form instance
+        if form.is_valid():
+            # Process valid form data
+            form.save()  # Or perform other actions with the cleaned data
+            # Redirect to success page or show confirmation message
+            return redirect('/')  # Replace with your success URL pattern name
+    else:
+        form = ProjectForm() #Like this we build an instance of ProjectForm
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+def updateProject(request, pk):
+    projectObj = Project.objects.get(id = pk)
+    form = ProjectForm(instance=projectObj)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=projectObj)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id = pk)
+
+    if request.method == 'POST':
+        project.delete()
+
+        return redirect('/')
+
+    return render(request, 'projects/delete.html', {'object':project})
