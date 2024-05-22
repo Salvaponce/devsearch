6da35dd1374be8621820e329818b9723cf8cfbf9
Salvador Ponce <salvaponce92@gmail.com>
2024-05-22 20:33:46 +0200

    adding a theme to user-profile and editing it

--- users/views.py
+++ users/views.py
@@ -5,3 +5,13 @@ from .models import Profile
 def profiles(request):
     profiles = Profile.objects.all()
     return render(request, 'users/profiles.html', {'profiles': profiles})
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id = pk)
+
+    topSkills = profile.skill_set.exclude(description__exact = "")
+    otherSkills = profile.skill_set.filter(description = "")
+
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
+
+    return render(request, 'users/user-profile.html', context)
